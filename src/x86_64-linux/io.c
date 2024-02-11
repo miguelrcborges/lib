d50077a1cb1c0e9b597e22baa533b4a95b7d4215
Miguel Borges <up202004481@edu.fe.up.pt>
2024-02-11 21:00:06 +0000

    me dummy

--- src/x86_64-linux/io.c
+++ src/x86_64-linux/io.c
@@ -1,17 +1,17 @@
 #include "lib/io.h"
 
-i64 open_syscall(const u8 *name, i32 flags, i32 mode);
+i64 open_syscall(const u8 *name, i32 flags);
 
 bool io_open(string file, u32 mode, usize *fd) {
 	static i32 flags_lookup[IO_MODES_COUNT] = {
-		[IO_READ]   = 1,
-		[IO_WRITE]  = 2, 
-		[IO_APPEND] = 3,
+		[IO_READ]   = 0,
+		[IO_WRITE]  = 1001, 
+		[IO_APPEND] = 2002,
 	};
 
 	int t_fd;
 	if (likely(file.str[file.len] == '\0')) {
-		t_fd = open_syscall(file.str, flags_lookup[mode], mode);
+		t_fd = open_syscall(file.str, flags_lookup[mode]);
 	} else {
 		u8 zeroed[4097];
 		usize i = 0;
@@ -19,7 +19,7 @@ bool io_open(string file, u32 mode, usize *fd) {
 			zeroed[i] = file.str[i];
 		} 
 		zeroed[i] = '\0';
-		t_fd = open_syscall(zeroed, flags_lookup[mode], mode);
+		t_fd = open_syscall(zeroed, flags_lookup[mode]);
 	}
 
 	*fd = (usize) t_fd;
